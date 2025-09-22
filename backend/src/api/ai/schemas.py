0566febd260473588274d7bf3c8b25920c743e4d
Carlos Vargas <carlos.vargas.godinez@ict.nl>
2025-09-22 16:16:37 +0200

    Last details to work with supervisor and agents

--- backend/src/api/ai/schemas.py
+++ backend/src/api/ai/schemas.py
@@ -5,3 +5,6 @@ class EmailMessageSchema(BaseModel):
     subject: str
     contents: str
     invalid_requests: bool | None = Field(default=False)
+
+class SupervisorMessageSchema(BaseModel):
+    content: str
--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -1,12 +1,18 @@
+import uuid
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
+from langgraph.checkpoint.memory import InMemorySaver
+
+
 from api.db import get_session
-from api.ai.schemas import EmailMessageSchema
+from api.ai.agents import get_supervisor
+from api.ai.schemas import EmailMessageSchema, SupervisorMessageSchema
 from api.ai.services import generate_email_message
 from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
 
 router = APIRouter()
+checkpointer = InMemorySaver()
 
 # /api/chats/
 @router.get("/")
@@ -24,17 +30,32 @@ def chat_list_messages(session: Session = Depends(get_session)):
 # HTTP POST -> payload = {"message": "Hello world"} -> {"message": "Hello World", "id": 1}
 # curl -X POST -d '{"message": "Hello world"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
 # curl -X POST -d '{"message": "Give me a summary of the Dutch weather"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
-@router.post("/", response_model=EmailMessageSchema)
+
+# curl -X POST -d '{"message": "Research why it is good to go outside and email me the results"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
+# curl -X POST -d '{"message": "Research what is the best type of food in different parts of the world and email me the results"}' -H "Content-Type: application/json" https://docker-api-fastapi-python-app-3jid9.ondigitalocean.app/api/chats/
+@router.post("/", response_model=SupervisorMessageSchema)
 def chat_create_message(
     payload:ChatMessagePayload,
     session: Session = Depends(get_session)
     ):
     data = payload.model_dump() # pydantic -> dict
-    print(data)
     obj = ChatMessage.model_validate(data)
     session.add(obj)
     session.commit()
-    # session.refresh(obj) # ensure id is added to the obj instance
-    # ready to store in the db
-    response = generate_email_message(payload.message)
-    return response
+    thread_id = uuid.uuid4()
+    supe = get_supervisor(checkpointer=checkpointer)
+    msg_data = {
+        "messages": [
+            {"role": "user",
+            "content": f"{payload.message}" 
+          },
+        ]
+    }
+    result = supe.invoke(msg_data, {"configurable": {"thread_id": thread_id}})
+
+    if not result:
+        raise HTTPException(status_code=400, detail="Error with supervisor")
+    messages = result.get("messages")
+    if not messages:
+        raise HTTPException(status_code=400, detail="Error with supervisor")
+    return messages[-1]
